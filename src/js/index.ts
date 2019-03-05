3f9779d37df3b7f87dfca8cb81ed54208a3f2561
Hasan Al-Basri <hasa0173@edu.easj.dk>
2019-03-05 11:34:01 +0100

    Lavet til opgave 3.1

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,28 @@
 //Her skal du skrive din typescriptkode
+var childNull = document.getElementById('0');
+console.log(childNull);
+
+var sundListe = document.getElementsByClassName('sundt');
+console.log(sundListe);
+
+var usundListe = document.getElementsByClassName('usundt');
+console.log(usundListe);
+
+var parrentList =  document.getElementsByTagName('ul')
+console.log(parrentList);
+
+var childList =  document.getElementsByTagName('li')
+console.log(childList);
+
+for (var i in parrentList) 
+{
+    console.log(i);
+}
+
+for (var io in childList) 
+{
+    console.log(io);
+}
+
+let id2tag = document.getElementById("2");
+id2tag.setAttribute("class","sundt");
